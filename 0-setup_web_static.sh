51a821d9ddefe9ff07a0202f89799410277debbe
hunterxcobby <solomonsefah13@gmail.com>
2024-02-02 07:57:07 +0000

    a Bash script that sets up your web servers for the deployment of web_static.

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# a Bash script that sets up your web servers for the deployment of web_static
+
+# Install Nginx if not already installed
+sudo apt-get update
+sudo apt-get -y install nginx
+
+# Create necessary folders
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+
+# Create fake HTML file
+echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Set ownership
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+config="
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    server_name _;
+
+    location / {
+        add_header X-Served-By $hostname;
+        proxy_pass http://127.0.0.1:5000;
+    }
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    $config
+}"
+
+sudo echo "$config" | sudo tee /etc/nginx/sites-available/default
+
+# Restart Nginx
+sudo service nginx restart
+
+exit 0
